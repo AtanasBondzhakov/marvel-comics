0f0411ee5a540c83fef8a4f2b2ee3230797e3401
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-03-09 01:35:01 +0200

    Add search page

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
+import styles from './Search.module.css';
+
+export default function Search() {
+    const [characterName, setCharacterName] = useState('');
+
+    const navigate = useNavigate();
+
+    const searchCharactersHandler = (e) => {
+        e.preventDefault();
+
+        navigate(`/characters/${characterName}`)
+    };
+
+    const handleChange = (e) => {
+        setCharacterName(e.target.value);
+    };
+
+    const handleReset = () => {
+
+    };
+
+    return (
+        <>
+            <form
+                className={styles.search}
+                onSubmit={searchCharactersHandler}
+            >
+                <input
+                    type="text"
+                    name="search"
+                    placeholder="Enter character name"
+                    value={characterName}
+                    onChange={handleChange}
+                />
+                <div className="buttons">
+                    <button type="submit">Search for Character</button>
+                    <button
+                        type="reset"
+                        className={styles.reset}
+                        onClick={handleReset}
+                    >Reset
+                    </button>
+                </div>
+            </form>
+        </>
+    );
+};
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function useFetch() {
-    const [data, setData] = useState();
+    const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
