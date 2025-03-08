144a049d6a34dee2f295c9bb0aaf8c023f607f62
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-03-08 23:51:02 +0200

    Install dependencies and add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from 'react-router'
+
 import './App.css'
 
+import Search from './components/search/Search.jsx'
+import Characters from './components/characters/Characters.jsx'
+
 function App() {
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <div className="app">
+                <Search />
+
+                <Routes>
+                    <Route path='/characters' element={<Characters />} />
+                </Routes>
+            </div>
+        </>
+    )
 }
 
 export default App
